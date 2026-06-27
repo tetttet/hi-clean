328ef5eb896fae45e0147d42eaa568daa388ac63
Turan-Yahya Gazizuly <turan-yahyagazizuly@172-2-1-244.lightspeed.dybhfl.sbcglobal.net>
2026-06-27 16:14:33 +0300

    updated

--- src/components/ui/services/price-data.ts
+++ src/components/ui/services/price-data.ts
@@ -690,4 +690,19 @@ export const frequencyOptions = [
   { id: "monthly", label: "Monthly", discount: 0.05 },
 ] as const;
 
-export const timeRangeOptions = ["09:00", "12:00", "15:00", "18:00", "21:00"];
+export const timeRangeOptions = [
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
+];
